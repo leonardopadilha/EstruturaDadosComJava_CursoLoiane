f37c4a79bb79392beae0134ef6cd59180a6d3721
leonardopadilha <leonardopadilha28@gmail.com>
2021-12-18 14:13:34 -0300

    Alterando método adiciona

--- src/test/java/estruturaDeDados/vetor/VetorTest.java
+++ src/test/java/estruturaDeDados/vetor/VetorTest.java
@@ -2,8 +2,14 @@ package estruturaDeDados.vetor;
 
 class VetorTest {
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(5);
-        vetor.adiciona("elemento 1");
+        try {
+            Vetor vetor = new Vetor(1);
+            vetor.adiciona("elemento 1");
+            vetor.adiciona("elemento 2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
