51755de4f52736e3a554b7e46a2dc8b81fc11985
leonardopadilha <leonardopadilha28@gmail.com>
2021-12-19 14:10:40 -0300

    Imprimindo o tamanho e elementos do vetor

--- src/test/java/estruturaDeDados/vetor/VetorTest.java
+++ src/test/java/estruturaDeDados/vetor/VetorTest.java
@@ -2,9 +2,13 @@ package estruturaDeDados.vetor;
 
 class VetorTest {
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(1);
-        vetor.adiciona("elemento 1");
-        vetor.adiciona("elemento 2");
+        Vetor vetor = new Vetor(10);
+        vetor.adiciona("Curso");
+        vetor.adiciona("Estrutura de Dados");
+
+        System.out.println(vetor.getTamanho());
+        System.out.println(vetor);
     }
+
 
 }
